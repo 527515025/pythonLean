8c1351242610eac4bd60c0a933c7fd5a5b252e1f
杨伊博 <527515025@qq.com>
2017-08-02 23:25:52 +0800

    添加一个神经层

--- AI/ts_06_activation_function.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# 目标 Tensorflow 中的 placeholder , placeholder 是 Tensorflow 中的占位符，暂时储存变量.
-# Tensorflow 如果想要从外部传入data, 那就需要用到 tf.placeholder(), 然后以这种形式传输数据 sess.run(***, feed_dict={input: **}).
-import tensorflow as tf
-
-
