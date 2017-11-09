4e3908ab1ea6c17e1a6f223325b0e8ab089f417d
杨伊博 <527515025@qq.com>
2017-11-10 00:46:31 +0800

    add drop out

--- AI/tensorflow_10_dropout.py
+++ AI/tensorflow_10_dropout.py
@@ -13,6 +13,8 @@ def add_layer(inputs, in_size, out_size,layer_name, activation_function=None):
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     # 定义Wx_plus_b, 即神经网络未激活的值(预测的值)。其中，tf.matmul()是矩阵的乘法。
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    #  用dropout来解决过拟合 问题
+    Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
     # activation_function ——激励函数（激励函数是非线性方程）为None时(线性关系)，输出就是当前的预测值——Wx_plus_b，
     # 不为None时，就把Wx_plus_b传到activation_function()函数中得到输出。
     if activation_function is None:
@@ -23,20 +25,8 @@ def add_layer(inputs, in_size, out_size,layer_name, activation_function=None):
         tf.summary.histogram(layer_name + '/outputs', outputs)
     return outputs
 
-def compute_accuracy(v_xs,v_ys):
-    # 定义 prediction 为全局变量
-    global prediction
-    # 将 xs data 在 prediction 中生成预测值，预测值也是一个 1*10 的矩阵 中每个值的概率，并不是一个0-9 的值，是0-9 每个值的概率 ，比如说3这个位置的概率最高，那么预测3就是这个图片的值
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
-    # 对比我的预测值y_pre 和真实数据 v_ys 的差别
-    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
-    # 计算我这一组数据中有多少个预测是对的，多少个是错的
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    # result 是一个百分比，百分比越高，预测越准确
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
-    return result   
-
-# keep_prob = tf.placeholder(tf.float32)
+# 保留数据 keep_prob
+keep_prob = tf.placeholder(tf.float32)
 xs = tf.placeholder(tf.float32,[None, 784]) #图像输入向量  每个图片有784 （28 ＊28） 个像素点
 ys = tf.placeholder(tf.float32, [None,10]) #每个例子有10 个输出
 
@@ -64,11 +54,10 @@ sess.run(init)
 for i in range(1000):
     #开始train，每次只取100张图片，免得数据太多训练太慢
     batch_xs, batch_ys = mnist.train.next_batch(50)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    # 丢弃百分之40 的数据
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
     if i % 50 == 0:
-        train_result = sess.run(merged, feed_dict={xs: batch_xs, ys: batch_ys})
-        test_result = sess.run(merged, feed_dict={xs: mnist.test.images, ys: mnist.test.labels})
+        train_result = sess.run(merged, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 1})
+        test_result = sess.run(merged, feed_dict={xs: mnist.test.images, ys: mnist.test.labels, keep_prob: 1})
         train_writer.add_summary(train_result,i)
         test_writer.add_summary(test_result,i)
-        print(compute_accuracy(
-            mnist.test.images, mnist.test.labels))
