cdfa9979934ef64592710bed69e0cac170d7745e
杨伊博 <527515025@qq.com>
2020-02-21 13:21:19 +0800

    add gzip

--- crawler/CrawlerBook.py
+++ crawler/CrawlerBook.py
@@ -1,6 +1,10 @@
 import urllib.request
 import os
 import re
+import gzip
+from io import BytesIO
+
+
 
 def writeFile(file,content):
     file.write(content)
@@ -12,18 +16,55 @@ def getContent(data):
 		return content
 	return
 
-try:
-	fo = open("/Users/yangyibo/Desktop/data.txt","a+",encoding="utf-8")
-	for num in range(34,82):
-		url = "https://www.shuhaige.com/55321/4211"+str(num)+".html"
-		print (url)
-		webPage=urllib.request.urlopen(url)
-		data = webPage.read().decode('utf-8')
-		data = data.replace("\n","")
-		# print(data)
-		content=getContent(data)
-		writeFile(fo,"\n第"+str(num-16)+"章节\n")
-		print("\n第"+str(num-16)+"章节\n")
-		writeFile(fo,content)
-except Exception as e:
-	print (e)
+def noGzipBook():
+	try:
+		fo = open("/Users/yangyibo/Desktop/data.txt","a+",encoding="utf-8")
+		for num in range(34,82):
+			# url = "https://www.shuhaige.com/55321/4211"+str(num)+".html"
+			url = "http://book.sbkk8.com/waiguo/feidu/164069.html"
+			webPage=urllib.request.urlopen(url)
+			print(webPage)
+			# data = webPage.read().decode('utf-8')
+			# data = data.replace("\n","")
+			# print(data)
+			# content=getContent(data)
+			# writeFile(fo,"\n第"+str(num-16)+"章节\n")
+			# print("\n第"+str(num-16)+"章节\n")
+			# writeFile(fo,content)
+	except Exception as e:
+		print (e)
+
+
+def getContentQiSu(data):
+	pattern = re.compile('<a href="/bookcase.php" >我的书架</a>(.*)投推荐票</a>',re.S)
+	for m in re.finditer(pattern,data):
+		content=m.group(1).replace("<br />","\n").replace("&nbsp;&nbsp;&nbsp;&nbsp;","    ")
+		return content
+	return
+
+
+def haveGzipBook(fo):
+	try:
+		for num in range(34,35):
+			url = "https://www.qisuu.la/du/5/5203/8508115.html"
+			webPage=urllib.request.urlopen(url)
+			html = webPage.read()
+			buff = BytesIO(html)
+			f = gzip.GzipFile(fileobj=buff)
+			data = f.read().decode('utf-8')
+			data = data.replace('\n','')
+			print(data)
+			# content=getContent(res)
+			# print(content)
+	except Exception as e:
+		print (e)
+
+
+
+if __name__ == "__main__":
+	# fo = open("/Users/yangyibo/Desktop/1.txt","a+",encoding="utf-8")
+	# haveGzipBook(fo)
+	noGzipBook()
+
+
+
